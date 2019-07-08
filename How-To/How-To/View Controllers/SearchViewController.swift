421f1c2531595b1f0e2d037e05f2960c79ebfebd
audreywelch <welch.audreyl@gmail.com>
2019-07-08 15:28:48 -0700

    Search View Controller basic UI completed

---
+++ How-To/How-To/View Controllers/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  How-To
+//
+//  Created by Audrey Welch on 7/8/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+}
